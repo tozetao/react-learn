df7d52dd03ab44cf78a9f018522df9c74808fc90
tozetao <tozetao@163.com>
2024-05-18 18:36:23 +0800

    connected-router-redux

--- src/my-connected-router-redux/lib/reducer.js
+++ src/my-connected-router-redux/lib/reducer.js
@@ -1,5 +1,12 @@
 import * as actionTypes from './actionTypes'
 
+/*
+
+需求1：route切换时，触发Store的dispatch，把路由相关信息存储到Store中。
+
+
+
+*/
 export default function(history) {
   const initState = {
     action: history.action,
